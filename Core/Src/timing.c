43ee3bc92b433c2fe073d591c01231175109bc93
Ulrik Hørlyk Hjort <Ulrik.Hoerlyk.Hjort@gmail.com>
2023-02-14 10:53:05 +0100

    Update timing.c

--- Core/Src/timing.c
+++ Core/Src/timing.c
@@ -5,19 +5,7 @@
 
 #include "timing.h"
 
-void reset_cnt() {
-    CoreDebug->DEMCR |= 0x01000000;
-    DWT->CYCCNT = 0;
-    DWT->CTRL = 0;
-}
-
-void start_cnt() { DWT->CTRL |= 0x00000001;}
-
-void stop_cnt() {  DWT->CTRL &= 0xFFFFFFFE;}
-
-unsigned int getCycles() { return DWT->CYCCNT;}
-
-void dwt_access_enable(unsigned ena) {
+void dwt_access_enable_func(unsigned ena) {
     uint32_t lsr = DWT->LSR;;
 
     if( (lsr & DWT_LSR_Present_Msk) != 0 ) {
